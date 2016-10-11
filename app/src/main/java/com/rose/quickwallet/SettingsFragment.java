6a22a06287a007bffa74cdceed7bb0637c54199d
rohanx96 <rohanx96@gmail.com>
2016-10-12 01:57:56 +0530

    feat: Add backup restore data feature
    Add backup otion in settings to copy database to external storage
    Add restore option in settings to restore backed up database
    Add write external storage permission

--- app/src/main/java/com/rose/quickwallet/SettingsFragment.java
+++ app/src/main/java/com/rose/quickwallet/SettingsFragment.java
@@ -1,22 +1,36 @@
 package com.rose.quickwallet;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 //import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
 
 //import com.rose.quickwallet.quickblox.Consts;
 //import com.rose.quickwallet.quickblox.GoCloudActivity;
 import com.rose.quickwallet.transactions.AddNewTransactionActivity;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
 
 
 /**
@@ -77,6 +91,39 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
             Intent change = new Intent(getActivity(), EnterPinActivity.class);
             change.setAction("CHANGE_PASSWORD");
             changePassword.setIntent(change);
+
+            Preference backup = findPreference("prefBackup");
+            backup.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    exportDB();
+                    return true;
+                }
+            });
+
+        Preference restore = findPreference("prefRestore");
+        restore.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                requestPermissions();
+                final AlertDialog dialog = new AlertDialog.Builder(getActivity()).create();
+                dialog.setMessage("This will replace your current application data. Are you sure you want to continue?");
+                dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        importDB();
+                    }
+                });
+                dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+                return true;
+            }
+        });
         }
 
     @Override
@@ -156,5 +203,92 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         //int intervalMillis = 5000; // 5 seconds
         AlarmManager alarm = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
         alarm.cancel(pIntent);
+    }
+
+    /** Copies the backed up database from external stroage to data*/
+    private void importDB() {
+
+        try {
+            //Log.i("Settings", " importing");
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+            if (sd.canWrite()) {
+                String newDBPath = "//data//" + "com.rose.quickwallet"
+                        + "//databases//" + "QuickWallet.db";
+                String oldDBPath = "QuickWalletData"; // From SD directory.
+
+                File newDB = new File(data, newDBPath);
+                File oldDB = new File(sd, oldDBPath);
+                if(oldDB.exists()) {
+                    FileChannel src = new FileInputStream(oldDB).getChannel();
+                    FileChannel dst = new FileOutputStream(newDB).getChannel();
+                    src.transferTo(0, src.size(), dst);
+                    src.close();
+                    dst.close();
+                    Snackbar.make(getView(),"Data restored successfully",Snackbar.LENGTH_LONG).show();
+                }
+                else{
+                    Snackbar.make(getView(),"Please create a backup first",Snackbar.LENGTH_LONG).show();
+                }
+
+            }
+            else {
+                Snackbar.make(getView(),"Please grant write external storage permission",Snackbar.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+
+            Toast.makeText(getActivity(), e.toString(), Toast.LENGTH_SHORT)
+                    .show();
+
+        }
+    }
+
+    /** Copies the database from data to external storage*/
+    private void exportDB() {
+        requestPermissions();
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            //File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+
+                String backupDBPath = "QuickWalletData";
+                File currentDB = getActivity().getDatabasePath("QuickWallet.db");
+                File backupDB = new File(sd, backupDBPath);
+
+                FileChannel src = new FileInputStream(currentDB).getChannel();
+                FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                dst.transferFrom(src, 0, src.size());
+                src.close();
+                dst.close();
+                Snackbar.make(getView(),"Backup Completed",Snackbar.LENGTH_LONG).show();
+
+            }
+            else {
+                Snackbar.make(getView(),"Please grant write external storage permission",Snackbar.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+
+            Snackbar.make(getView(),"Backup Failed",Snackbar.LENGTH_LONG).show();
+
+        }
+    }
+
+    public void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                android.support.v7.app.AlertDialog.Builder dialogBuilder = new android.support.v7.app.AlertDialog.Builder(getActivity());
+                dialogBuilder.setMessage("The write external storage permission is required to create a copy of the database on sd card")
+                        .setPositiveButton(getString(R.string.dlg_ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                            }
+                        });
+                dialogBuilder.show();
+            } else
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
     }
 }
